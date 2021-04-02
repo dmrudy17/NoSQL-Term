04208d0106e97572bab648b1314df5d329d27c69
dmrudy17 <dmr136@zips.uakron.edu>
2021-04-02 10:09:24 -0400

    Add BootstrapVue and Start Swiping card

---
+++ client/src/components/HomeCard.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <b-card
+            style="max-width: 20rem; height: 15rem; margin: 0 auto; float: none;"
+            class="mb-2"
+            border-variant="dark"
+        >
+            <div class="card-text">
+                <b-card-text>
+                Start Swiping Now!
+                </b-card-text>
+            </div>
+                <router-link to="/post">
+                    <b-icon icon="chevron-double-right" variant="primary" style="width: 100px; height: 100px" animation="cylon"></b-icon>
+                </router-link>
+        </b-card>
+    </div>
+</template>
+
+<style scoped>
+    .mb-2 {
+        background-color: #d3f7f6;
+    }
+    .card-text {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #878787;
+        font-size: x-large;
+    }
+</style>
