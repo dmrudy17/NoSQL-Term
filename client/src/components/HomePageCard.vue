35a99afc974cf7f42106894889d85481d05b8cc0
sgun7 <srg97@zips.uakron.edu>
2021-04-03 22:35:20 -0400

    Add homepage stuff

---
+++ client/src/components/HomePageCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="main-header">
+    <md-card class="md-primary" md-with-hover md-theme="green-card">
+      <md-ripple>
+        <md-card-media-cover md-text-scrim>
+          <md-card-media md-ratio="16:9">
+            <img :src="dogLink" height="400px" />
+          </md-card-media>
+        </md-card-media-cover>
+        <div class="head-color">
+          <h1>Welcome to our Website</h1>
+          <md-button
+            class="md-raised md-primary"
+            :md-ripple="false"
+            @click="loadDogImage"
+            >Check Out More</md-button
+          >
+        </div>
+      </md-ripple>
+    </md-card>
+  </div>
+</template>
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      dogLink: "",
+    };
+  },
+  methods: {
+    loadDogImage() {
+      axios
+        .get("https://dog.ceo/api/breeds/image/random")
+        .then((response) => {
+          this.dogLink = response.data.message;
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
+  created() {
+    this.loadDogImage();
+  },
+};
+</script>
+<style scoped>
+.md-card {
+  height: 650px;
+  width: 920px;
+  margin: 3%;
+  border-radius: 15px;
+}
+.head-color {
+  height: 30px;
+}
+.md-card-media-cover {
+  border-radius: 15px;
+}
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-import Footer from "./components/Footer.vue"
+import Footer from "./components/Footer.vue";
+import HomePageCard from "./components/HomePageCard.vue";
+import { MdButton, MdCard } from "vue-material/dist/components";
+import "vue-material/dist/vue-material.min.css";
+import "vue-material/dist/theme/default.css";
 
-Vue.component('main-footer', Footer);
+Vue.use("md-button", MdButton);
+Vue.use("md-card", MdCard);
+
+Vue.component("main-footer", Footer);
+Vue.component("main-card", HomePageCard);
 
 Vue.config.productionTip = false;
 
