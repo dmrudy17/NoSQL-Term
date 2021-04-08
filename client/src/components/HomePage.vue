970f3d90cb7df55a6258ee7cd50152f3a007b8e2
sgun7 <srg97@zips.uakron.edu>
2021-04-07 21:11:27 -0400

    New

--- client/src/components/HomePage.vue
+++ client/src/components/HomePage.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home-foot">
     <h2>Home Page Placeholder</h2>
+    <swipe-card />
     <image-slide></image-slide>
   </div>
 </template>
--- client/src/main.js
+++ client/src/main.js
@@ -2,22 +2,24 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import Footer from "./components/Footer.vue";
+import SwipeCard from "./components/SwipeCard.vue";
+import NavBar from "./components/NavBar.vue";
 import imageSlide from "./components/ImageSlide.vue";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
-// register the plugin on vue
+
 import Toasted from "vue-toasted";
 
-// Import Bootstrap an BootstrapVue CSS files (order is important)
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
+import "./styles/app.scss";
 
-Vue.use(Toasted);
-// Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue);
-// Optionally install the BootstrapVue icon components plugin
 Vue.use(IconsPlugin);
+Vue.use(Toasted);
 
 Vue.component("main-footer", Footer);
+Vue.component("swipe-card", SwipeCard);
+Vue.component("navbar", NavBar);
 Vue.component("image-slide", imageSlide);
 
 Vue.config.productionTip = false;
