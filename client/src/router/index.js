4c72dea6419f934b7fbb6d10e08f3d8dede77d74
dmrudy17 <dmr136@zips.uakron.edu>
2021-04-08 21:44:49 -0400

    Create page for adding posts with form layout

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import Router from "vue-router";
 import HomePage from "@/components/HomePage";
 import PostComponent from "@/components/PostComponent";
+import AddPage from "@/components/AddPage";
 
 Vue.use(Router);
 
@@ -9,5 +10,6 @@ export default new Router({
   routes: [
     { path: "/", component: HomePage },
     { path: "/post", component: PostComponent },
+    { path: "/add", component: AddPage },
   ],
 });
