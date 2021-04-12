b842b8805915acbc8270dd3a2e596ee1e1b971bd
sgun7 <srg97@zips.uakron.edu>
2021-04-12 01:09:56 -0400

    Card mostly working, commit for rebase

--- client/src/components/SwipeCard.vue
+++ client/src/components/SwipeCard.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="swipecard-container">
-    <b-card
-      class="mb-2"
-      border-variant="dark"
-    >
+    <b-card class="mb-2" border-variant="dark">
       <div class="card-text">
         <b-card-text>
-        Start Swiping Now!
+          Start Swiping Now!
         </b-card-text>
       </div>
+<<<<<<< HEAD
         <router-link to="/add">
             <b-icon icon="chevron-double-right" variant="info" style="width: 100px; height: 100px" animation="cylon"></b-icon>
         </router-link>
+=======
+      <router-link to="/post">
+        <b-icon
+          icon="chevron-double-right"
+          variant="info"
+          style="width: 100px; height: 100px"
+          animation="cylon"
+        ></b-icon>
+      </router-link>
+>>>>>>> Card mostly working, commit for rebase
     </b-card>
   </div>
 </template>
 
 <style scoped>
+<<<<<<< HEAD
     .mb-2 {
         background-color: #d3f7f6;
         max-width: 25rem;
@@ -37,4 +46,22 @@
         font-size: x-large;
         font-weight: bold;
     }
+=======
+.mb-2 {
+  background-color: #d3f7f6;
+  max-width: 20rem;
+  height: 15rem;
+  margin: 0 auto;
+  float: none;
+}
+.card-text {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #878787;
+  font-size: x-large;
+  font-weight: bold;
+}
+>>>>>>> Card mostly working, commit for rebase
 </style>
